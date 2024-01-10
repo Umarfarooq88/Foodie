46d1c4368e1a2906eb580115822bdbdcb899d99e
Umar farooq <umarat372@gmial.com>
2024-01-10 23:31:23 +0530

    Nested tag

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
-const heading = React.createElement
-("h1",
-{id:"heading"},
-"Learning React");             //heading returns a object and still its not a h1 tag.
-        const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent = React.createElement(
+    "div",
+    {id:"parent"}, 
+    React.createElement("div",{id:"child"},React.createElement("h1",{id:"new tag"}, "Nested tag")));
 
-        root.render(heading);// this statement renders the object and thn makes it as a h1 tag and show it in the browser.
+
+
+// const heading = React.createElement
+// ("h1",
+// {id:"heading"},
+// "Learning React");             //heading returns a object and still its not a h1 tag.
+//         const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//         root.render(parent);// this statement renders the object and thn makes it as a h1 tag and show it in the browser.
