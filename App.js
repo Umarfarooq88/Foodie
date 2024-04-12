6d9bdca4910a38445d018cb3d7634842303923b4
Umar farooq <umarat372@gmial.com>
2024-04-12 21:58:40 +0530

    Foodie start

--- App.js
+++ App.js
@@ -1,49 +1,29 @@
 import React from 'react';
-import ReactDOM  from 'react-dom';
+import ReactDOM from 'react-dom';
 
-//JSX
-const heading= <h1>Namaste REACT 🚀</h1>;
-// the above JSX is transcompiled to React.createElement and the to HTML Element and then it is rendered in the browser using BABEL.
-// JSX=> REact eElement => HTML Element
-
-
-// react element ----- starts with lower case letters
-             //------- for single line use "parenthesis"
-             //--------for double line use return is der and use "flower brackets" 
-const headingg=(
- <h1>  This is react element.</h1>
-);
-
-
-
-/*
-react component
- ------- for single line use "parenthesis"
- --------for double line use return is der and use "flower brackets" 
-to render use "</>"
-*/
-const Component1=()=>(
-     <h1  className='headd'>This is react component 1 </h1>
-);
-
-const data="300"; // if any infected API call is stored in data then when its put in component , JSX will take care of that and provies secuirity. 
-
-// with flower brackets and /*component composition*/
-const Component2=()=>{
-    return(
-   <div>
-   <Component1/> 
-   {Component1()} /* calling the function */
-   <Component1/><Component1/>
-   {data}
-   <h1  className='headd'>This is react component 2</h1> 
-   <h1>React  is awesome!🚀 </h1>
-   </div>
-    );
-    
+const Header = () => {
+  return (
+    <div className='flex '>
+      <img src="https://img.freepik.com/premium-vector/online-food-app-icon-food-shop-location-logo-also-online-resturent-location-template_608547-155.jpg" alt="Restaurant Logo" className="w-40 h-auto  mr-4" />
+      <div className=' items-center text-blue-500'>
+        <ul className="pt-16 pl-96  flex space-x-20 items-center">
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
+const AppLayout = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
 
-const root=ReactDOM.createRoot(document.getElementById('root'));
-    root.render(<Component2/>);
-    
+const root = document.getElementById('root');
+ReactDOM.render(<AppLayout />, root);
